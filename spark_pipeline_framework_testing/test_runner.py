39857af019d65bb5f4a9d7841dc14bcaedb37ecb
imranq2 <imranq2@hotmail.com>
2020-10-12 14:59:43 -0700

    add text for implementations

--- spark_pipeline_framework_testing/test_runner.py
+++ spark_pipeline_framework_testing/test_runner.py
@@ -6,16 +6,15 @@ from typing import List
 class SparkPipelineFrameworkTestRunner:
     @staticmethod
     def run_tests(folder_path: Path) -> None:
-        # iterate through sub_folders trying to find folders called input and output
-        # file_list: List[str] = list_files(folder_path=folder_path)
-
-        # folder_list: List[str] = list_folders(folder_path=folder_path)
-
-        # folder_suffix_list: List[str] = [
-        #     PurePath(folder_path).name for folder_path in folder_list
-        # ]
+        # iterate through sub_folders trying to find folders that contain input and output folders
         testable_folder_list: List[str] = testable_folders(folder_path=folder_path)
         print(testable_folder_list)
+        # for each of them
+        # for each file in input folder, load into a view in Spark (use name of file without extension as name of view)
+        # turn path into transformer name
+        # call transformer
+        # for each file in output folder, loading into a view in Spark (prepend with "expected_")
+        # Do a data frame compare on each
 
 
 def testable_folders(folder_path: Path) -> List[str]:
