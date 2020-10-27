fa6093e30388d7d4359c1b09235350fc6d283dd6
imranq2 <imranq2@hotmail.com>
2020-10-27 10:02:44 -0700

    added test

---
+++ tests/library/features/medstar/practitioner/v1/test_folder.py
@@ -0,0 +1,13 @@
+from pathlib import Path
+
+from pyspark.sql import SparkSession
+
+from spark_pipeline_framework_testing.test_runner import SparkPipelineFrameworkTestRunner
+
+
+def test_folder(spark_session: SparkSession) -> None:
+    data_dir: Path = Path(__file__).parent.joinpath('./')
+
+    SparkPipelineFrameworkTestRunner.run_tests(
+        spark_session=spark_session, folder_path=data_dir
+    )
