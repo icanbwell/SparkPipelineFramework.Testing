780cc320d3864cab5e96931f0dac968e5851a206
imranq2 <imranq2@hotmail.com>
2020-12-05 23:29:48 -0800

    convert jsonl to json for easier comparison

--- tests/library/features/people/json_feature/test_json_feature.py
+++ tests/library/features/people/json_feature/test_json_feature.py
@@ -5,7 +5,7 @@ from pyspark.sql import SparkSession
 from spark_pipeline_framework_testing.test_runner import SparkPipelineFrameworkTestRunner
 
 
-def test_folder(spark_session: SparkSession) -> None:
+def test_json_feature(spark_session: SparkSession) -> None:
     data_dir: Path = Path(__file__).parent.joinpath('./')
 
     SparkPipelineFrameworkTestRunner.run_tests(
