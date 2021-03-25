8fc21e9983255dc357aba408951ae2e86fd0f202
kyle paul <kyle@icanbwell.com>
2021-03-25 14:03:09 -0700

    fix bug

--- spark_pipeline_framework_testing/test_runner.py
+++ spark_pipeline_framework_testing/test_runner.py
@@ -156,12 +156,14 @@ class SparkPipelineFrameworkTestRunner:
 
             if check_output:
                 # write out any missing schemas
-                output_tables: List[Table] = [
-                    table
-                    for table in spark_session.catalog.listTables("default")
-                    if ignore_views_for_output
-                    and table.name not in ignore_views_for_output
-                ]
+                output_tables: List[Table] = spark_session.catalog.listTables("default")
+
+                if ignore_views_for_output is not None:
+                    output_tables = [
+                        table
+                        for table in output_tables
+                        if table.name not in ignore_views_for_output
+                    ]
 
                 output_schema_folder: Path = Path(testable_folder).joinpath(
                     "output_schema"
