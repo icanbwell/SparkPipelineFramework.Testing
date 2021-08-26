81f3c7dcd8fc4f2c3309d6a28b8214efad7fdeb6
Amin <amin.rashidi@gmail.com>
2021-08-25 21:32:20 -0500

    fix inputs and more tests

--- tests/library/features/v2/response_json/test_request_response_call.py
+++ tests/library/features/v2/response_json/test_request_response_call.py
@@ -26,7 +26,10 @@ def test_request_response_call(spark_session: SparkSession) -> None:
     mock_client = MockServerFriendlyClient(mock_server_url)
     mock_client.clear(test_name)
     input_file = input_types.FileInput()
-    request = input_types.HttpJsonRequest(response_data_folder="request_response_call")
+    request1 = input_types.HttpJsonRequest(response_data_folder="request_response_call")
+    request2 = input_types.HttpJsonRequest(
+        response_data_folder="request_response_call2"
+    )
     logger = get_logger(__name__)
     SparkPipelineFrameworkTestRunnerV2(
         spark_session=spark_session,
@@ -37,12 +40,16 @@ def test_request_response_call(spark_session: SparkSession) -> None:
         auto_find_helix_transformer=False,
         helix_transformers=[FeaturesComplexFeature],
         mock_client=mock_client,
-        test_inputs=[input_file, request],
+        test_inputs=[input_file, request1, request2],
         temp_folder="output/temp",
     ).run_test2()
 
     with open(test_path.joinpath("request_response_call/provider.json")) as f:
         content = json.load(f)
+    with open(test_path.joinpath("request_response_call2/provider2.json")) as f:
+        content2 = json.load(f)
 
-    response: Response = requests.get(f"{mock_server_url}/{test_name}/provider.json")
-    assert response.json() == content
+    response: Response = requests.get(f"{mock_server_url}/{test_name}/provider?page=1")
+    assert response.json() == content["request_result"]
+    response = requests.get(f"{mock_server_url}/{test_name}/provider2?page=1")
+    assert response.json() == content2["request_result"]
