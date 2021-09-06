e0b7de47209756b4ba8c1e756d3cb297abd09e16
Amin <amin.rashidi@gmail.com>
2021-09-06 15:44:30 -0500

    fix helix param update

--- spark_pipeline_framework_testing/test_runner_v2.py
+++ spark_pipeline_framework_testing/test_runner_v2.py
@@ -120,7 +120,7 @@ class SparkPipelineFrameworkTestRunnerV2:
         )
         if self.helix_pipeline_parameters:
             standard_parameters.update(self.helix_pipeline_parameters)
-            self.helix_pipeline_parameters = standard_parameters
+        self.helix_pipeline_parameters = standard_parameters
 
     def run_test2(self) -> None:
         assert self.temp_folder_path
