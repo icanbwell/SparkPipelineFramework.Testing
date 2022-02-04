fbb317a2454888ba53706024770329b9f68b8966
Michael Vidal <michael.vidal@icanbwell.com>
2022-02-03 21:33:52 -0500

    added support for handling xml input files

--- tests/library/pipeline/fixed_width_pipeline/v1/test_fixed_width_pipeline.py
+++ tests/library/pipeline/fixed_width_pipeline/v1/test_fixed_width_pipeline.py
@@ -1,7 +1,12 @@
 from pathlib import Path
 
 from pyspark.sql import SparkSession
+from pyspark.sql.types import StringType, IntegerType
 from spark_pipeline_framework.logger.yarn_logger import get_logger
+from spark_pipeline_framework.transformers.framework_fixed_width_loader.v1.framework_fixed_width_loader import (
+    ColumnSpec,
+)
+
 from spark_pipeline_framework_testing.mockserver_client.mockserver_client import (
     MockServerFriendlyClient,
 )
@@ -20,7 +25,27 @@ def test_fixed_width_pipeline(
     clean_spark_session(session=spark_session)
     data_dir: Path = Path(__file__).parent.joinpath("./")
     test_name = "test_fixed_width_pipeline"
-    test_input = input_types.FileInput()
+
+    columns = [
+        ColumnSpec(column_name="id", start_pos=1, length=3, data_type=StringType()),
+        ColumnSpec(
+            column_name="some_date", start_pos=4, length=8, data_type=StringType()
+        ),
+        ColumnSpec(
+            column_name="some_string",
+            start_pos=12,
+            length=3,
+            data_type=StringType(),
+        ),
+        ColumnSpec(
+            column_name="some_integer",
+            start_pos=15,
+            length=4,
+            data_type=IntegerType(),
+        ),
+    ]
+
+    test_input = input_types.FileInput(fixed_width_columns=columns, row_tag="book")
     logger = get_logger(__name__)
 
     mock_server_url = "http://mock-server:1080"
@@ -28,7 +53,10 @@ def test_fixed_width_pipeline(
     mock_client.clear(f"/{test_name}/")
     mock_client.expect_default()
 
-    params = {"input_file_path": f"{data_dir.joinpath('input').joinpath('test.txt')}"}
+    params = {
+        "input_file_path": f"{data_dir.joinpath('input').joinpath('test.txt')}",
+        "input_xml_path": f"{data_dir.joinpath('input').joinpath('books.xml')}",
+    }
 
     SparkPipelineFrameworkTestRunnerV2(
         spark_session=spark_session,
@@ -42,7 +70,8 @@ def test_fixed_width_pipeline(
         helix_pipeline_parameters=params,
     ).run_test2()
 
-    result = spark_session.table("my_view")
+    result = spark_session.table("test")
+    result_xml = spark_session.table("books")
     assert result.count() == 2
     assert result.collect()[0][0] == "001"
     assert result.collect()[1][0] == "002"
@@ -52,3 +81,5 @@ def test_fixed_width_pipeline(
     assert result.collect()[1][2] == "me"
     assert result.collect()[0][3] == 1234
     assert result.collect()[1][3] == 5678
+    assert result_xml.count() == 12
+    assert len(result_xml.columns) == 7
