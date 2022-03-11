97f420db4779cbcd1282d4c07cc6b6b853603499
imranq2 <imranq2@hotmail.com>
2022-03-11 11:05:04 -0800

    update packages

--- setup.py
+++ setup.py
@@ -51,7 +51,7 @@ setup(
         "sparkpipelineframework>=1.0.11",
         "sparkdataframecomparer>=1.0.0",
         "deprecated>=1.2.12",
-        "helix.fhir.client.sdk>=1.0.0",
+        "helix.fhir.client.sdk>=1.0.2",
     ],
     name=package_name,
     version=version,
