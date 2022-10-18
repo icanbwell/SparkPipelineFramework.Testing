84257553b0e2ce28b42b08501eb0aa9105f4d725
imranq2 <imranq2@hotmail.com>
2022-10-17 19:14:42 -0700

    log pipeline before running it

--- spark_pipeline_framework_testing/test_runner_v2.py
+++ spark_pipeline_framework_testing/test_runner_v2.py
@@ -259,6 +259,9 @@ class SparkPipelineFrameworkTestRunnerV2:
             df: DataFrame = self.spark_session.createDataFrame(
                 self.spark_session.sparkContext.emptyRDD(), schema
             )
+            progress_logger.write_to_log(
+                my_instance.__class__.__name__, str(my_instance)
+            )
             my_instance.transform(df)
 
     @staticmethod
