b6490cd6e42385de066da47885c5f2b392453224
Denis <104531605+dcanbwell@users.noreply.github.com>
2024-09-05 15:04:50 -0500

    changed to the proper way of stripping out the file extension to see which views should be ignored based on the ignore_views_for_output setting

--- spark_pipeline_framework_testing/test_classes/validator_types.py
+++ spark_pipeline_framework_testing/test_classes/validator_types.py
@@ -735,8 +735,8 @@ class OutputFileValidator(Validator):
         output_files: List[str] = [
             f
             for f in listdir(self.output_folder_path)
-            if isfile(join(self.output_folder_path, f)) # and file without extension is not in ignore_views_for_output
-            and f.split(".")[0] not in self.ignore_views_for_output
+            if isfile(join(self.output_folder_path, f))
+            and Path(f).stem not in self.ignore_views_for_output
         ]
         views_found: List[str] = []
         data_frame_exceptions: List[SparkDataFrameComparerException] = []
