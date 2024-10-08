3bfd32dfb53aed05b34bba11c41d977998c149ab
007sunny <sunny.singhal@icanbwell.com>
2024-10-08 11:29:42 +0530

    EFS-1228 Update to latest fhir server image

--- tests/library/features/v2/doctor_feature/practitioner_fail_on_fhir_validation/v1/test_practitioner_fail_on_fhir_validation.py
+++ tests/library/features/v2/doctor_feature/practitioner_fail_on_fhir_validation/v1/test_practitioner_fail_on_fhir_validation.py
@@ -2,6 +2,7 @@ from pathlib import Path
 
 import pytest
 from mockserver_client.mockserver_client import MockServerFriendlyClient
+import requests
 from spark_pipeline_framework.logger.yarn_logger import get_logger
 from spark_pipeline_framework_testing.validators.fhir_validator import FhirValidator
 
@@ -40,12 +41,28 @@ def test_practitioner_fail_on_fhir_validation(spark_session: SparkSession) -> No
         "mock_server_url": mock_server_url,
     }
 
+    fhir_server_auth_config = requests.get(
+        url="http://fhir:3000/.well-known/smart-configuration"
+    )
+    assert (
+        fhir_server_auth_config.status_code == 200
+    ), f"{fhir_server_auth_config.status_code}: {fhir_server_auth_config.text}"
+    access_token_response = requests.post(
+        url=fhir_server_auth_config.json()["token_endpoint"],
+        data="grant_type=client_credentials&client_id=bwell-client-id&client_secret=bwell-secret",
+        headers={"Content-Type": "application/x-www-form-urlencoded"},
+    )
+    assert (
+        access_token_response.status_code == 200
+    ), f"{access_token_response.status_code}: {access_token_response.text}"
+
     test_validator = FhirValidator(
         related_inputs=test_fhir,
         related_file_inputs=test_input,
         mock_server_url=mock_server_url,
         test_name=test_name,
         fhir_validation_url="http://fhir:3000/4_0_0",
+        fhir_server_access_token=f"{access_token_response.json()['access_token']}",
     )
 
     with pytest.raises(AssertionError, match=r"Failed validation for resource*"):
