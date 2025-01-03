b3ad1326f07e847336c876df4468ba8fd79dc78e
Imran Qureshi <imranq2@hotmail.com>
2025-01-03 15:22:47 -0800

    improve logging

--- spark_pipeline_framework_testing/test_classes/validator_types.py
+++ spark_pipeline_framework_testing/test_classes/validator_types.py
@@ -243,21 +243,16 @@ class MockCallValidator(Validator):
                     )
                     failure_message += f"{compare_files_text}\n"
                     for content_not_matched_exception in content_not_matched_exceptions:
-                        failure_message += f"----------- {content_not_matched_exception.expected_file_path} -----------\n"
+                        failure_message += f"-----------  {content_not_matched_exception.url} File: {content_not_matched_exception.expected_file_path} -----------\n"
                         for difference in content_not_matched_exception.differences:
                             failure_message += f"{difference}\n"
 
                 else:
                     failure_message += f"{len(content_not_matched_exceptions)} requests did not match: \n"
-                    msg = "\n".join(
-                        [
-                            f"expected: {c.expected_json}\nactual: {c.actual_json}"
-                            for c in content_not_matched_exceptions
-                        ]
-                    )
+                    msg = "\n".join([str(c) for c in content_not_matched_exceptions])
                     failure_message += f"{msg}\n"
                     for content_not_matched_exception in content_not_matched_exceptions:
-                        failure_message += f"----------- {content_not_matched_exception.expected_file_path} -----------\n"
+                        failure_message += f"-----------  {content_not_matched_exception.url} File:  {content_not_matched_exception.expected_file_path} -----------\n"
                         for difference in content_not_matched_exception.differences:
                             failure_message += f"{difference}\n"
 
@@ -327,10 +322,14 @@ class MockCallValidator(Validator):
                 all_expectations_str = "\n".join(
                     [f"{expectation.request}" for expectation in all_expectations]
                 )
-                logger.info(f"\n ---- ALL EXPECTATIONS -------\n{all_expectations_str}")
+                logger.info(
+                    f"\n ---- ALL EXPECTATIONS -------\n{all_expectations_str}\n--- END EXPECTATIONS ---\n"
+                )
 
                 all_requests_str = "\n".join([f"{request}" for request in all_requests])
-                logger.info(f"\n ---- ALL REQUESTS -------\n{all_requests_str}")
+                logger.info(
+                    f"\n ---- ALL REQUESTS -------\n{all_requests_str}\n--- END REQUESTS ---\n"
+                )
 
                 pytest.fail(test_failure_message)
             elif warning_message:
@@ -513,20 +512,15 @@ class MockRequestValidator(Validator):
                     )
                     failure_message += f"{compare_files_text}\n"
                     for content_not_matched_exception in content_not_matched_exceptions:
-                        failure_message += f"----------- {content_not_matched_exception.expected_file_path} -----------\n"
+                        failure_message += f"-----------  {content_not_matched_exception.url} File {content_not_matched_exception.expected_file_path} -----------\n"
                         for difference in content_not_matched_exception.differences:
                             failure_message += f"{difference}\n"
                 else:
-                    failure_message += f"{len(content_not_matched_exceptions)} requests did not match: \n"
-                    msg = "\n".join(
-                        [
-                            f"{c.method} {c.url}\nexpected: {c.expected_json}\nactual: {c.actual_json}"
-                            for c in content_not_matched_exceptions
-                        ]
-                    )
+                    failure_message += f"{len(content_not_matched_exceptions)} requests did not match: \n\n"
+                    msg = "\n".join([str(c) for c in content_not_matched_exceptions])
                     failure_message += f"{msg}\n"
                     for content_not_matched_exception in content_not_matched_exceptions:
-                        failure_message += f"----------- {content_not_matched_exception.expected_file_path} -----------\n"
+                        failure_message += f"----------- {content_not_matched_exception.url} File: {content_not_matched_exception.expected_file_path} -----------\n"
                         for difference in content_not_matched_exception.differences:
                             failure_message += f"{difference}\n"
 
@@ -597,10 +591,14 @@ class MockRequestValidator(Validator):
                 all_expectations_str = "\n".join(
                     [f"{expectation.request}" for expectation in all_expectations]
                 )
-                logger.info(f"\n ---- ALL EXPECTATIONS -------\n{all_expectations_str}")
+                logger.info(
+                    f"\n ---- ALL EXPECTATIONS -------\n{all_expectations_str}\n--- END EXPECTATIONS ---\n"
+                )
 
                 all_requests_str = "\n".join([f"{request}" for request in all_requests])
-                logger.info(f"\n ---- ALL REQUESTS -------\n{all_requests_str}")
+                logger.info(
+                    f"\n ---- ALL REQUESTS -------\n{all_requests_str}\n--- END REQUESTS ---\n"
+                )
 
                 pytest.fail(test_failure_message)
             elif warning_message:
