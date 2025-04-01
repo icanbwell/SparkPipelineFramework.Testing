6361f20943052e87615759f874e507f6d9dfe029
Imran Qureshi <imranq2@hotmail.com>
2025-03-31 22:03:35 -0700

    add register.py

--- conftest.py
+++ conftest.py
@@ -1,11 +1,29 @@
-from typing import Any
+from typing import Any, Generator
 
 import pytest
 from pyspark.sql.session import SparkSession
 
 from create_spark_session import create_spark_session
+from spark_pipeline_framework_testing.register import register
 
 
 @pytest.fixture(scope="session")
 def spark_session(request: Any) -> SparkSession:
     return create_spark_session(request)
+
+
+@pytest.fixture(scope="session", autouse=True)
+def run_before_each_test() -> Generator[None, Any, None]:
+    # This code will run before every test
+    # print("Setting up something before each test")
+    # You can do setup operations here
+    # For example, initializing databases, clearing caches, etc.
+    print("Setting up before each test")
+
+    register()
+
+    # Optional: You can yield if you want to do tear down after the test
+    yield
+
+    # Optional teardown code here
+    print("Cleaning up after each test")
